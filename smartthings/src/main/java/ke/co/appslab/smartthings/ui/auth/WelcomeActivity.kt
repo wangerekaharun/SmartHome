21761ab8b3e11dfb960ff575dd22c9135a006d4f
wangerekaharun <harunwangereka@gmail.com>
2019-02-28 13:28:38 +0300

    finished androidThings kit welcome ui

---
+++ smartthings/src/main/java/ke/co/appslab/smartthings/ui/auth/WelcomeActivity.kt
@@ -0,0 +1,21 @@
+package ke.co.appslab.smartthings.ui.auth
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import ke.co.appslab.smartthings.R
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+
+class WelcomeActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        getStartedRel.setOnClickListener {
+            val thingsConnectionIntent = Intent(this, ThingsConnectionActivity::class.java)
+            startActivity(thingsConnectionIntent)
+        }
+    }
+}
