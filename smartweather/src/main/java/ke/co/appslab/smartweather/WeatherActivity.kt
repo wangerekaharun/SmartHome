a46417c416f1adeded4dec976b2d40cc05e9f173
wangerekaharun <harunwangereka@gmail.com>
2019-03-17 10:51:19 +0300

    added weather module

---
+++ smartweather/src/main/java/ke/co/appslab/smartweather/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package ke.co.appslab.smartweather
+
+import android.app.Activity
+import android.os.Bundle
+
+class WeatherActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
