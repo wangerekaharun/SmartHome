a340820275f99d1f68df6dc2f987fc436eecd32b
wangerekaharun <harunwangereka@gmail.com>
2019-03-30 18:32:52 +0300

    added logic to display notifications when app is in foreground

--- app/src/main/java/ke/co/appslab/smarthome/ui/firebase/InstanceIdService.kt
+++ app/src/main/java/ke/co/appslab/smarthome/ui/firebase/InstanceIdService.kt
@@ -1,9 +1,16 @@
 package ke.co.appslab.smarthome.ui.firebase
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.navigation.NavDeepLinkBuilder
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import ke.co.appslab.smarthome.R
 import ke.co.appslab.smarthome.models.UserLog
 import ke.co.appslab.smarthome.utils.Constants.USER_LOGS
 
@@ -25,7 +32,38 @@ class InstanceIdService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
         super.onMessageReceived(remoteMessage)
         remoteMessage?.data?.isNotEmpty()?.let {
+            sendNotification(remoteMessage.notification?.title, remoteMessage.notification?.body)
 
         }
+    }
+
+    private fun sendNotification(title: String?, body: String?) {
+
+        val pendingIndent = NavDeepLinkBuilder(applicationContext)
+            .setGraph(R.navigation.nav_graph)
+            .setDestination(R.id.homeFragment)
+            .createPendingIntent()
+
+        val notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val channelId = getString(R.string.default_notification_channel_id)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_smarthome)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setContentIntent(pendingIndent)
+            .setAutoCancel(true)
+            .setSound(notificationSound)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
     }
 }
